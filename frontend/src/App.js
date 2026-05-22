f4ec8fef1a69648a3140ba4079ae579cc1a7ea5a
sandeep <you@example.com>
2026-05-22 13:21:15 +0530

    connect backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ function App() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    await fetch("http://13.51.169.146:5000/contact", {
+    await fetch("http://13.49.230.214:5000/contact", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(form)
